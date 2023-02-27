e07ac7cb1768542a9019d0554cbba31d40bc763a
shivakmuddam25 <115982123+shivakmuddam25@users.noreply.github.com>
2023-02-27 21:36:38 +0530

    Update 1_Plotting.py

--- Streamlit-Intern1/pages/1_Plotting.py
+++ Streamlit-Intern1/pages/1_Plotting.py
@@ -3,14 +3,16 @@ import pandas as pd
 import pickle
 
 st.header("Welcome to Data Visualization")
-#df = pd.read_csv("./LifeExpectancyData.csv")
+df = pd.read_csv("../LifeExpectancyData.csv")
+st.write(df)
 
 # with open("data.sav", 'wb') as f:
 #     pickle.dump(df, f)
 
-df = pickle.load(open('../data.sav', 'rb'))
+#df = pickle.load(open('../data.sav', 'rb'))
 
-st.dataframe(df)
+
+#st.dataframe(df)
 
 # Display the various charts
 
