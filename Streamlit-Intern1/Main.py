1747663a7d88afd76b354c22ca93ca3a4aed7744
shivakmuddam25 <115982123+shivakmuddam25@users.noreply.github.com>
2023-02-27 20:56:09 +0530

    Add files via upload

---
+++ Streamlit-Intern1/Main.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+import pandas as pd
+import streamlit as st
+
+st.subheader(" Home Page ")
+st.write("Hi, this is Shiva, an ardent professional to become a Data Scientist.")
+st.write("""This is project is all about the Life Expectancy details of each country based
+on the year. Also the data contains a lot of information about the consequences that the 
+potential reasons could be the reduction of the human survival. 
+
+Based on the data provided, it is our duty to train the model, considering it
+as a Regression task, to find the prediction of the life expectancy based on the 
+data provided. 
+
+As of now, lets visualize the data, as we cook the model meanwhile.
+
+Thanks for reading my post.
+    
+         """)
+
+
+# def intro():
+#     st.header("Intro")
+#
+#
+# def plotting_demo():
+#     st.header("plotting_demo")
+
+
+# def data_frame_demo():
+#     st.header("data_frame_demo")
+#
+#
+# def mapping_demo():
+#     st.header("mapping_demo")
+
+
+# page_names_to_funcs = {
+#     "—": intro,
+#     "Plotting Demo": plotting_demo
+#
+# }
+#
+# demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+# page_names_to_funcs[demo_name]()
---
+++ Streamlit-Intern1/pages/1_Plotting.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header("Welcome to Data Visualization")
+df = pd.read_csv("LifeExpectancyData.csv")
+st.dataframe(df)
+
+# Display the various charts
+
