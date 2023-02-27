f647389ccc51148b995d190728bb42854efd9165
shivakmuddam25 <115982123+shivakmuddam25@users.noreply.github.com>
2023-02-27 21:07:05 +0530

    Update 1_Plotting.py

--- Streamlit-Intern1/pages/1_Plotting.py
+++ Streamlit-Intern1/pages/1_Plotting.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.header("Welcome to Data Visualization")
-df = pd.read_csv("LifeExpectancyData.csv")
+df = pd.read_csv("./LifeExpectancyData.csv")
 st.dataframe(df)
 
 # Display the various charts
