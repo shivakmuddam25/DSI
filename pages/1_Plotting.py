77b3d2a451c15ce6126e50a296f2e62a00b09318
shivakmuddam25 <115982123+shivakmuddam25@users.noreply.github.com>
2023-02-28 09:00:45 +0530

    Add files via upload

---
+++ pages/1_Plotting.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.header("Data Visualization")
+df = pd.read_csv("./Data/LifeExpectancyData.csv")
+# with open("data.sav", 'wb') as f:
+#     pickle.dump(df, f)
+
+# df = pickle.load(open('./data.sav', 'rb'))
+# st.write(df)
+st.dataframe(df)
+
+# Display the various charts
+
